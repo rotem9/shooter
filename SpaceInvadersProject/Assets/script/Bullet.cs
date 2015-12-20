bf428cb5655f61f38b99f93eac9f0efab9bfc85f
rotem <r.shaul9@yahoo.com>
2015-12-20 14:08:03 +0200

    added full power trail

--- SpaceInvadersProject/Assets/script/Bullet.cs
+++ SpaceInvadersProject/Assets/script/Bullet.cs
@@ -7,9 +7,13 @@ public class Bullet : MonoBehaviour {
 	public GameObject explosion;
 	public static int power;
 	public static bool playhitmonster;
+	public static TrailRenderer powerTrail;
 	
 	// Use this for initialization
 	void Start () {
+		powerTrail = GetComponent <TrailRenderer> ();
+		powerTrail.enabled = false;
+
 		blah = GetComponent <Rigidbody2D> ();
 		Vector3 sp = Camera.main.WorldToScreenPoint (transform.position);
 		Vector3 dir = (Input.mousePosition - sp).normalized;
@@ -19,7 +23,14 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//transform.position = new Vector2(transform.position.x + 0.2f, transform.position.y);
-		
+		if (PlayerMovement.fired) {
+			Bullet.powerTrail.enabled = false;
+		}
+
+		if (PlayerMovement.currPower >= 0.95f) {
+			Bullet.powerTrail.enabled = true;
+		}
+
 		if(transform.position.x > 4 || transform.position.y < -3.5 || transform.position.y > 3.5 || transform.position.x < -4.7)
 		{
 			Destroy(gameObject);
